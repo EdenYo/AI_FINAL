d0c1d41b37c3f1ecc9f061b9a1ff63c37b4bc89f
EdenYo <88801785+EdenYo@users.noreply.github.com>
2021-08-12 10:05:26 +0300

    Update First_assistant.py

--- First_assistant.py
+++ First_assistant.py
@@ -1,5 +1,7 @@
 import Gmm
 import os
 if __name__ == '__main__':
+    # read the data
     csv_file_path = os.path.join(os.path.dirname(__file__), 'Data', 'mushrooms_data.txt')
+    # run gmm 
     Gmm.main(csv_file_path, False)
